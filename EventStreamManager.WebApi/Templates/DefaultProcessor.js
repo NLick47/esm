e0a9d9633228e2088a5cede35544d6583be4c61a
Nlick47 <2247717951@qq.com>
2026-03-25 20:36:35 +0800

    更改脚本模板代码

--- EventStreamManager.WebApi/Templates/DefaultProcessor.js
+++ EventStreamManager.WebApi/Templates/DefaultProcessor.js
@@ -21,6 +21,15 @@ class ProcessResult {
         this.requestInfo = null;
         return this;
     }
+
+
+    setNoSend(reason = '') {
+        this.needToSend = false;
+        this.reason = reason || '仅执行脚本，无需发送';
+        this.error = null;
+        this.requestInfo = null;
+        return this;
+    }
 }
 
 /**
@@ -32,21 +41,6 @@ function process(data) {
     //tip 当前使用的库函数均为对脚本引擎进行的函数注入，如需额外库函数需要实现相应函数插件
     // 在这里编写你的数据处理逻辑
     const result = new ProcessResult();
-    
     console_log('收到数据:', data);
-
-    // 示例：根据数据来源判断
-    //if (data.rows.strSource === "体检") {
-    //    return result.setFailure('非门诊或住院数据，跳过处理');
-    //}
-
-    // 示例：成功处理并返回请求信息
-    //const requestInfo = {
-    //    patientId: data.rows.patientId,
-    //   patientName: data.rows.patientName,
-    //    examType: data.rows.examType
-    //};
-    //const json = json_stringify(requestInfo);
-    //return result.setSuccess(json);
-    return result.setSuccess('');
+    return result.setNoSend('示例');
 }
